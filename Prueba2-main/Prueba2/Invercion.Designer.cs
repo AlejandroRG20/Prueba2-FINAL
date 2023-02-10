562752538ee089cde1566415b778016fb4de46b8
AlejandroRG <111157823+AlejandroRG20@users.noreply.github.com>
2023-02-10 16:38:58 -0600

    Add files via upload

---
+++ Prueba2-main/Prueba2/Invercion.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Prueba2
+{
+    partial class Invercion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.data = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Datos = new System.Windows.Forms.GroupBox();
+            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.data)).BeginInit();
+            this.Datos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Invercion Inicial";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tipo de plazo";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(135, 55);
+            this.maskedTextBox1.Mask = "0000000000000000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(121, 26);
+            this.maskedTextBox1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1 - 3 Años",
+            "4 - 10 Años",
+            "10  + Años"});
+            this.comboBox1.Location = new System.Drawing.Point(135, 124);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // data
+            // 
+            this.data.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.data.Location = new System.Drawing.Point(111, 265);
+            this.data.Name = "data";
+            this.data.RowTemplate.Height = 25;
+            this.data.Size = new System.Drawing.Size(440, 264);
+            this.data.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 634);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(179, 634);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Limpiar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(305, 634);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Salir";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Datos
+            // 
+            this.Datos.Controls.Add(this.maskedTextBox3);
+            this.Datos.Controls.Add(this.label3);
+            this.Datos.Controls.Add(this.maskedTextBox2);
+            this.Datos.Controls.Add(this.label12);
+            this.Datos.Controls.Add(this.maskedTextBox1);
+            this.Datos.Controls.Add(this.label1);
+            this.Datos.Controls.Add(this.label2);
+            this.Datos.Controls.Add(this.comboBox1);
+            this.Datos.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Datos.Location = new System.Drawing.Point(35, 39);
+            this.Datos.Name = "Datos";
+            this.Datos.Size = new System.Drawing.Size(553, 210);
+            this.Datos.TabIndex = 8;
+            this.Datos.TabStop = false;
+            this.Datos.Text = "Datos";
+            // 
+            // maskedTextBox3
+            // 
+            this.maskedTextBox3.Location = new System.Drawing.Point(404, 106);
+            this.maskedTextBox3.Mask = "0000000000000000";
+            this.maskedTextBox3.Name = "maskedTextBox3";
+            this.maskedTextBox3.Size = new System.Drawing.Size(121, 26);
+            this.maskedTextBox3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(284, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Egreso Mensual";
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(404, 52);
+            this.maskedTextBox2.Mask = "0000000000000000";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(121, 26);
+            this.maskedTextBox2.TabIndex = 5;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(284, 55);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(112, 20);
+            this.label12.TabIndex = 4;
+            this.label12.Text = "Ingreso Mensual";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(24, 311);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Ing. Inicial";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(35, 340);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(27, 20);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Ing";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(32, 369);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(30, 20);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Egr";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label10.Location = new System.Drawing.Point(24, 389);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(53, 20);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Utilidad";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label11.Location = new System.Drawing.Point(21, 409);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(71, 20);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "Impuestos";
+            // 
+            // Invercion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(658, 669);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.Datos);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.data);
+            this.Name = "Invercion";
+            this.Text = "Invercion";
+            this.Load += new System.EventHandler(this.Invercion_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.data)).EndInit();
+            this.Datos.ResumeLayout(false);
+            this.Datos.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private MaskedTextBox maskedTextBox1;
+        private ComboBox comboBox1;
+        private DataGridView data;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private GroupBox Datos;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private MaskedTextBox maskedTextBox2;
+        private Label label12;
+        private Label label10;
+        private Label label11;
+        private MaskedTextBox maskedTextBox3;
+        private Label label3;
+    }
+}
---
+++ Prueba2-main/Prueba2/Invercion.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prueba2
+{
+    public partial class Invercion : Form
+    {
+        public Invercion()
+        {
+            InitializeComponent();
+        }
+
+        private void Invercion_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte fila;
+            byte columna;
+            double b, aux;
+            double IngI,Ing, Egr,Egre,Ingr, Ut,h,g,imp;
+
+            if (maskedTextBox1.Text == "")
+            {
+                MessageBox.Show("No se aceptan espacios, digite un ingreso inicial");
+                maskedTextBox1.Focus();
+            }
+            else if (comboBox1.Text == " ")
+            {
+                MessageBox.Show("No se aceptan espacios, digite un plazo");
+                comboBox1.Focus();
+            }
+            else if (maskedTextBox2.Text == "")
+            {
+                MessageBox.Show("No se aceptan espacios, digite su salario mensual");
+                maskedTextBox2.Focus();
+            }else if (maskedTextBox3.Text=="")
+            {
+                MessageBox.Show("No se aceptan espacios, digite su Egreso mensual");
+                maskedTextBox3.Focus();
+            }
+            else
+            {
+                if (comboBox1.Text == "1 - 3 Años")
+                {
+                    IngI = Convert.ToDouble(maskedTextBox1.Text);
+                    Ing = Convert.ToDouble(maskedTextBox2.Text);
+                    Egr = Convert.ToDouble(maskedTextBox3.Text);
+                    if (IngI < 100 || IngI > 25000)
+                    {
+                        MessageBox.Show("Rango entre 100 A 25000");
+                        maskedTextBox1.Text = "";
+                        maskedTextBox1.Focus();
+                    }
+                    else
+                    {
+
+
+                        if (Egr > Ing)
+                        {
+                            MessageBox.Show("El Egreso no puede ser mayor al Ingreso");
+                            maskedTextBox3.Focus();
+                        }
+                        else
+                        {
+                            fila = 6; columna = 4;
+                            Ingr = Ing * 12;
+                            Egre = Egr * 12;
+                            data.ColumnCount = columna;
+                            data.RowCount = fila;
+
+                            for (int i = 0; i < data.ColumnCount; i++)
+                            {
+                                data[i, 0].Value = "Año " + (i);
+                            }
+                            data[0, 1].Value = IngI;
+
+
+
+                            for (int i = 1; i < data.ColumnCount; i++)
+                            {
+                                data[i, 2].Value = Ingr;
+                                Ingr = (Ingr * 0.10) + Ingr;
+                            }
+
+                            for (int n = 1; n < data.ColumnCount; n++)
+                            {
+                                data[n, 3].Value = Egre;
+                                Egre = (Egre * 0.10) + Egre;
+
+                            }
+                            h = Ing * 12;
+                            g = Egr * 12;
+                            for (int l = 1; l < data.ColumnCount; l++)
+                            {
+                                Ut = h - g;
+                                data[l, 4].Value = Ut;
+                                h = (h * 0.10) + h;
+                                g = (g * 0.10) + g;
+
+
+                            }
+                            h = Ing * 12;
+                            g = Egr * 12;
+                            for (int m = 1; m < data.ColumnCount; m++)
+                            {
+
+                                Ut = h - g;
+                                h = (h * 0.10) + h;
+                                g = (g * 0.10) + g;
+                                imp = Ut * 0.30;
+                                data[m, 5].Value = imp;
+
+                            }
+                        }
+                    }
+                }else if(comboBox1.Text=="4 - 10 Años")
+                {
+                    IngI = Convert.ToDouble(maskedTextBox1.Text);
+                    Ing = Convert.ToDouble(maskedTextBox2.Text);
+                    Egr = Convert.ToDouble(maskedTextBox3.Text);
+                    if (IngI < 100 || IngI > 25000)
+                    {
+                        MessageBox.Show("Rango entre 100 A 25000");
+                        maskedTextBox1.Text = "";
+                        maskedTextBox1.Focus();
+                    }
+                    else
+                    {
+                        if (Egr > Ing)
+                        {
+                            MessageBox.Show("El Egreso no puede ser mayor al Ingreso");
+                            maskedTextBox3.Focus();
+                        }
+                        else
+                        {
+                            fila = 6; columna = 11;
+                            Ingr = Ing * 12;
+                            Egre = Egr * 12;
+                            data.ColumnCount = columna;
+                            data.RowCount = fila;
+
+                            for (int i = 0; i < data.ColumnCount; i++)
+                            {
+                                data[i, 0].Value = "Año " + (i);
+                            }
+                            data[0, 1].Value = IngI;
+
+
+
+                            for (int i = 1; i < data.ColumnCount; i++)
+                            {
+                                data[i, 2].Value = Ingr;
+                                Ingr = (Ingr * 0.10) + Ingr;
+                            }
+
+                            for (int n = 1; n < data.ColumnCount; n++)
+                            {
+                                data[n, 3].Value = Egre;
+                                Egre = (Egre * 0.10) + Egre;
+
+                            }
+                            h = Ing * 12;
+                            g = Egr * 12;
+                            for (int l = 1; l < data.ColumnCount; l++)
+                            {
+                                Ut = h - g;
+                                data[l, 4].Value = Ut;
+                                h = (h * 0.10) + h;
+                                g = (g * 0.10) + g;
+
+
+                            }
+                            h = Ing * 12;
+                            g = Egr * 12;
+                            for (int m = 1; m < data.ColumnCount; m++)
+                            {
+
+                                Ut = h - g;
+                                h = (h * 0.10) + h;
+                                g = (g * 0.10) + g;
+                                imp = Ut * 0.30;
+                                data[m, 5].Value = imp;
+
+                            }
+                        }
+                    }
+                }else if(comboBox1.Text=="10  + Años")
+                {
+                    IngI = Convert.ToDouble(maskedTextBox1.Text);
+                    Ing = Convert.ToDouble(maskedTextBox2.Text);
+                    Egr = Convert.ToDouble(maskedTextBox3.Text);
+                    if (IngI < 100 || IngI > 25000)
+                    {
+                        MessageBox.Show("Rango entre 100 A 25000");
+                        maskedTextBox1.Text = "";
+                        maskedTextBox1.Focus();
+                    }
+                    else
+                    {
+                        if (Egr > Ing)
+                        {
+                            MessageBox.Show("El Egreso no puede ser mayor al Ingreso");
+                            maskedTextBox3.Focus();
+                        }
+                        else
+                        {
+                            fila = 6; columna = 16;
+                            Ingr = Ing * 12;
+                            Egre = Egr * 12;
+                            data.ColumnCount = columna;
+                            data.RowCount = fila;
+
+                            for (int i = 0; i < data.ColumnCount; i++)
+                            {
+                                data[i, 0].Value = "Año " + (i);
+                            }
+                            data[0, 1].Value = IngI;
+
+
+
+                            for (int i = 1; i < data.ColumnCount; i++)
+                            {
+                                data[i, 2].Value = Ingr;
+                                Ingr = (Ingr * 0.10) + Ingr;
+                            }
+
+                            for (int n = 1; n < data.ColumnCount; n++)
+                            {
+                                data[n, 3].Value = Egre;
+                                Egre = (Egre * 0.10) + Egre;
+
+                            }
+                            h = Ing * 12;
+                            g = Egr * 12;
+                            for (int l = 1; l < data.ColumnCount; l++)
+                            {
+                                Ut = h - g;
+                                data[l, 4].Value = Ut;
+                                h = (h * 0.10) + h;
+                                g = (g * 0.10) + g;
+                            }
+                            h = Ing * 12;
+                            g = Egr * 12;
+                            for (int m = 1; m < data.ColumnCount; m++)
+                            {
+
+                                Ut = h - g;
+                                h = (h * 0.10) + h;
+                                g = (g * 0.10) + g;
+                                imp = Ut * 0.30;
+                                data[m, 5].Value = imp;
+                            }
+                        }
+                    }
+                }
+                else if (comboBox1.Text != "1 - 3" && comboBox1.Text != "4 - 10" && comboBox1.Text != "10 +")
+                {
+                    MessageBox.Show("Digite un plazo valido");
+                    comboBox1.Text = "";
+                    comboBox1.Focus();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            maskedTextBox1.Text = "";
+            maskedTextBox2.Text = "";
+            maskedTextBox3.Text = "";
+            comboBox1.Text = "";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
+
+
+
